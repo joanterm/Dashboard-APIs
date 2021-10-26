8a29faf1625e666846c2e78f7728d85b19ea82b4
joanterm <jo_joanarg@yahoo.com>
2021-10-26 15:22:06 -0400

    Added cryptocurrency UI and fetched data from crypto API

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 const myKeyAccess = "dFICnddE6ZFgwGzvuXlFywQRN9JF2z4p8ALs-QBxwoQ"
 const body = document.querySelector("#body")
 const imageAuthor = document.querySelector(".image-author")
+const cryptoName = document.querySelector(".crypto-name")
+const cryptoIcon = document.querySelector(".crypto-icon")
 
-
+FETCHES API FOR RANDOM BACKGROUND IMAGE
 const getRandomImage = () => {
-    fetch(`https://api.unsplash.com/photos/random/?query=nature
-    &orientation=landscape&client_id=${myKeyAccess}`)
+    fetch(`https://api.unsplash.com/photos/random/?query=nature&orientation=landscape&client_id=${myKeyAccess}`)
     .then((response) => {
         if(!response.ok) {
-            throw Error(`Error code is : ${response.status}`)
+            throw Error(`Error code is: ${response.status}`)
         }
         return response.json()
     })
@@ -26,5 +27,26 @@ const getRandomImage = () => {
     })
 }
 getRandomImage()
+
+// FETCHES API FOR CRYPTOCURRENCY COIN
+const getCryptoCoin = () => {
+    fetch("https://api.coingecko.com/api/v3/coins/strong")
+    .then((responseCoin) => {
+        if (!responseCoin.ok) {
+            throw Error(`Error code is: ${responseCoin.status}`)
+        }
+        return responseCoin.json()
+    })
+    .then((dataCoin) => {
+        console.log(dataCoin)
+        cryptoName.innerHTML = dataCoin.name
+        cryptoIcon.src = dataCoin.image.small
+    })
+    .catch((errCoin) => {
+        console.log(errCoin)
+        cryptoName.innerHTML="Data unavailable"
+    })
+}
+getCryptoCoin()
 
 
